e0506e93fe4d78334747b5713db459f61e214ba4
KalabSisaySE <kyimusic11@gmail.com>
2023-01-23 20:37:44 +0300

    fix task-2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -36,15 +36,17 @@ class Server:
         for num in [page, page_size]:
             assert isinstance(num, int) and num > 0
         indexes = index_range(page=page, page_size=page_size)
-        if len(data_set) >= indexes[1]:
+        len_ds = len(data_set)
+        if len_ds >= indexes[1]:
             return [data_set[index] for index in range(indexes[0], indexes[1])]
+        elif len_ds >= indexes[0] and len_ds < indexes[1]:
+            return [data_set[index] for index in range(indexes[0], len_ds)]
 
         return []
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
         """returns a dict of page info based on `page` and `page_size`"""
         data_set = self.dataset()
-        pages = len(data_set) / page_size
         current_dataset = self.get_page(page=page, page_size=page_size)
 
         return {
@@ -53,5 +55,5 @@ class Server:
             "data": current_dataset,
             "next_page": (page + 1) if (current_dataset != []) else None,
             "prev_size": (page - 1) if (page - 1 > 0) else None,
-            "total_pages": round(pages),
+            "total_pages": math.ceil(len(data_set) / page_size),
         }
