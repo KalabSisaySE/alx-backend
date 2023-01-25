6f7450a972cbf80c94cc88e3ae97775b95d4bbb4
KalabSisaySE <kyimusic11@gmail.com>
2023-01-25 10:38:58 +0300

    update task-3 implementation

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -8,7 +8,10 @@ BaseCaching = __import__("base_caching").BaseCaching
 class LRUCache(BaseCaching):
     """implements a caching system based on the LRU cache replacement policy"""
 
-    recent_access = []
+    def __init__(self) -> None:
+        """instantiates a new LRUCache object"""
+        super().__init__()
+        self.recent_access = []
 
     def put(self, key, item):
         """stores items in the cache and
@@ -19,23 +22,23 @@ class LRUCache(BaseCaching):
             if not self.cache_data.get(key):
                 # if limit reached
                 if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    least_recent = LRUCache.recent_access[0]
+                    least_recent = self.recent_access[0]
                     print("DISCARD: {}".format(least_recent))
                     del self.cache_data[least_recent]
-                    LRUCache.recent_access.remove(least_recent)
+                    self.recent_access.remove(least_recent)
 
             self.cache_data[key] = item
             # if item already exists remove it first
             # to avoid duplicates
-            if key in LRUCache.recent_access:
-                LRUCache.recent_access.remove(key)
-            LRUCache.recent_access.append(key)
+            if key in self.recent_access:
+                self.recent_access.remove(key)
+            self.recent_access.append(key)
 
     def get(self, key):
         """retrieves items from the cache based on key, if it exists"""
         if key in list(self.cache_data.keys()):
             # update the recent access list
-            if key in LRUCache.recent_access:
-                LRUCache.recent_access.remove(key)
-            LRUCache.recent_access.append(key)
+            if key in self.recent_access:
+                self.recent_access.remove(key)
+            self.recent_access.append(key)
         return self.cache_data.get(key)
